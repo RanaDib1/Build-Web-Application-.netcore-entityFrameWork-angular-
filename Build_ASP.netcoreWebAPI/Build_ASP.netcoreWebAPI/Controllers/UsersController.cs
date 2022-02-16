4404e68fc5f7658122fdb0f0fdbd419e293fe567
Rana Haj Diab <rhajdiab@Netways.com>
2022-02-16 21:31:10 +0300

    first commit

---
+++ Build_ASP.netcoreWebAPI/Build_ASP.netcoreWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Build_ASP.netcoreWebAPI.Data;
+using Build_ASP.netcoreWebAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Build_ASP.netcoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        //api/Users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            return user;
+        }
+    }
+}
